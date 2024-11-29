1ab605c0b9c04e8e8bb7e5d74ac5dfb77adb7031
GaelAlburo <gael.a24@outlook.com>
2024-11-29 12:56:42 -0600

    test(front-home): add text to the home page to test the font

--- front/app/page.jsx
+++ front/app/page.jsx
@@ -1,8 +1,15 @@
+"use client";
+import { Container, Typography, useTheme } from "@mui/material";
 
 
 export default function Home() {
+  const theme = useTheme();
+
   return (
-    <>
-    </>
+    <Container maxWidth="md" sx={{border: "1px solid red"}}>
+      <Typography variant="h1">
+        How are you
+      </Typography>
+    </Container>
   );
 }
