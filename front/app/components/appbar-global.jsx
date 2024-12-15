b150feba3ab286bb7d0f36a825934229748b4850
GaelAlburo <gael.a24@outlook.com>
2024-12-14 21:05:21 -0600

    feat(navbar): update navbar to not take vertical space

--- front/app/components/appbar-global.jsx
+++ front/app/components/appbar-global.jsx
@@ -62,162 +62,169 @@ export default function AppBarGlobal() {
       }
     }
       return (
-        <AppBar position="sticky"
-          sx={{backgroundColor: "rgba(255,255,255, 0.2)", backdropFilter: "blur(50px)", borderBottom: "1px solid rgba(0,0,0,0.8)"}}
-        >
-          <Toolbar>
+        <Box position={"relative"}> {/* Box with relative position to make the appbar not take space */}
+          {/* Fixed position makes it stay at the rop of the screen */}
+          <AppBar position="fixed"
+            sx={{
+              backgroundColor: "rgba(255,255,255, 0.2)", 
+              backdropFilter: "blur(50px)", 
+              borderBottom: "1px solid rgba(0,0,0,0.8)"
+            }}
+          >
+            <Toolbar>
 
-            {/* Logo on big screens */}
-            <CloudIcon sx={{ display: { xs: "none", md: "flex" }, mr: 2, color: "black" }} />
-            <Typography
-              variant="h6"
-              noWrap
-              component={Link}
-              href="/"
-              sx={{
-                display: { xs: "none", md: "flex" },
-                mr: 1,
-                fontWeight: 700,
-                letterSpacing: ".2rem",
-                color: "black",
-                textDecoration: "none",
-              }}
-            >
-              Cloud Bridge
-            </Typography>
-            
-            {/* Menu on small screens */}
-            <Box sx={{flexGrow: 1, display: {sx: "flex", md: "none"}}}>
-
-              <IconButton
-                size="large"
-                aria-label="open drawer"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="black"
-              >
-                <MenuIcon />
-              </IconButton>
-
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
+              {/* Logo on big screens */}
+              <CloudIcon sx={{ display: { xs: "none", md: "flex" }, mr: 2, color: "black" }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component={Link}
+                href="/"
+                sx={{
+                  display: { xs: "none", md: "flex" },
+                  mr: 1,
+                  fontWeight: 700,
+                  letterSpacing: ".2rem",
+                  color: "black",
+                  textDecoration: "none",
                 }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{display: {xs: "block", md: "none"}}}
               >
+                Cloud Bridge
+              </Typography>
+              
+              {/* Menu on small screens */}
+              <Box sx={{flexGrow: 1, display: {sx: "flex", md: "none"}}}>
+
+                <IconButton
+                  size="large"
+                  aria-label="open drawer"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="black"
+                >
+                  <MenuIcon />
+                </IconButton>
+
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{display: {xs: "block", md: "none"}}}
+                >
+                  {navItems.map((item) => {
+                    return(
+                      <MenuItem key={item.label} >
+                        <Typography sx={{textAlign: "center", color: "black", textDecoration: "none"}} component={Link} href={item.href}>
+                          {item.label}
+                        </Typography>
+                      </MenuItem>
+                    )
+                  })}
+                </Menu>
+
+              </Box>
+              
+              {/* Logo on small screens */}
+              <CloudIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1, color: "black" }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component={Link}
+                href="/"
+                sx = {{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontWeight: 700,
+                  letterSpacing: ".2rem",
+                  color: "black",
+                  textDecoration: "none",
+                }}
+              >
+                Cloud Bridge
+              </Typography>
+              
+              {/* Navigation items on big screns */}
+              <Box sx={{ flexGrow: 1, ml: 4, display: { xs: 'none', md: 'flex' } }}>
                 {navItems.map((item) => {
-                  return(
-                    <MenuItem key={item.label} >
-                      <Typography sx={{textAlign: "center", color: "black", textDecoration: "none"}} component={Link} href={item.href}>
-                        {item.label}
-                      </Typography>
-                    </MenuItem>
+                  return (
+                    <Button
+                      key={item.label}
+                      component={Link}
+                      href={item.href}
+                      onClick={handleCloseNavMenu}
+                      sx={{my: 2, mr: 4, color: "inherit", display: "block", color: "black"}}
+                    >
+                      {item.label}
+                    </Button>
                   )
                 })}
-              </Menu>
+              </Box>
+              
+              {/* User options menu */}
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open user options">
+                  <IconButton onClick={handleOpenUserMenu} sx={{p: 0, color: "black"}}>
+                    <FaceIcon />
+                  </IconButton>
+                </Tooltip>
 
-            </Box>
-            
-            {/* Logo on small screens */}
-            <CloudIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1, color: "black" }} />
-            <Typography
-              variant="h5"
-              noWrap
-              component={Link}
-              href="/"
-              sx = {{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontWeight: 700,
-                letterSpacing: ".2rem",
-                color: "black",
-                textDecoration: "none",
-              }}
-            >
-              Cloud Bridge
-            </Typography>
-            
-            {/* Navigation items on big screns */}
-            <Box sx={{ flexGrow: 1, ml: 4, display: { xs: 'none', md: 'flex' } }}>
-              {navItems.map((item) => {
-                return (
-                  <Button
-                    key={item.label}
-                    component={Link}
-                    href={item.href}
-                    onClick={handleCloseNavMenu}
-                    sx={{my: 2, mr: 4, color: "inherit", display: "block", color: "black"}}
-                  >
-                    {item.label}
-                  </Button>
-                )
-              })}
-            </Box>
-            
-            {/* User options menu */}
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open user options">
-                <IconButton onClick={handleOpenUserMenu} sx={{p: 0, color: "black"}}>
-                  <FaceIcon />
-                </IconButton>
-              </Tooltip>
+                <Menu
+                  sx={{mt: 4}}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {
+                  isAuthenticated === 'true' ?
+                  (
+                    settings.map((setting) => {
+                    return (
+                      <MenuItem key={setting.label} onClick={() => handleCloseUserMenu(setting.label)} >
+                        <Typography sx={{textAlign: "center", color: "black", textDecoration: "none"}} component={Link} href={setting.href}>
+                          {setting.label}
+                        </Typography>
+                      </MenuItem>
+                    )
+                  }))
+                  :
+                  (
+                    AccountOptions.map((setting) => {
+                    return (
+                      <MenuItem key={setting.label} onClick={() => handleCloseUserMenu(setting.label)} >
+                        <Typography sx={{textAlign: "center", color: "black", textDecoration: "none"}} component={Link} href={setting.href}>
+                          {setting.label}
+                        </Typography>
+                      </MenuItem>
+                    )
+                  }))
+                  }
+                </Menu>
+              </Box>
 
-              <Menu
-                sx={{mt: 4}}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {
-                isAuthenticated === 'true' ?
-                (
-                  settings.map((setting) => {
-                  return (
-                    <MenuItem key={setting.label} onClick={() => handleCloseUserMenu(setting.label)} >
-                      <Typography sx={{textAlign: "center", color: "black", textDecoration: "none"}} component={Link} href={setting.href}>
-                        {setting.label}
-                      </Typography>
-                    </MenuItem>
-                  )
-                }))
-                :
-                (
-                  AccountOptions.map((setting) => {
-                  return (
-                    <MenuItem key={setting.label} onClick={() => handleCloseUserMenu(setting.label)} >
-                      <Typography sx={{textAlign: "center", color: "black", textDecoration: "none"}} component={Link} href={setting.href}>
-                        {setting.label}
-                      </Typography>
-                    </MenuItem>
-                  )
-                }))
-                }
-              </Menu>
-            </Box>
-
-          </Toolbar>
-        </AppBar>
+            </Toolbar>
+          </AppBar>
+        </Box>
       );
 }
