1836015abd80834e1ed123a3a9f4c1f66f4e34a7
GaelAlburo <gael.a24@outlook.com>
2024-12-15 21:44:07 -0600

    feat(layout): add meta tag for responsiveness

--- front/app/layout.jsx
+++ front/app/layout.jsx
@@ -3,7 +3,6 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "../styles/global-theme";
 import AppBarGlobal from "./components/appbar-global";
 import FooterGlobal from "./components/footer-global";
-import { useState } from "react";
 import { AuthProvider } from "./contexts/SessionContext";
 
 // export const metadata = {
@@ -16,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Funnel+Sans:ital@0;1&display=swap" rel="stylesheet"/>
